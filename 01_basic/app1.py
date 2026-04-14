b1cbd7a421061b7aeab96961cec8a7e2e312aae8
Ved <ved00684@gmail.com>
2026-04-14 17:32:53 +0530

    02 Template added

---
+++ 01_basic/app1.py
@@ -0,0 +1,24 @@
+from logging import debug
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user",name="Admin!")) #! Return route with Admin! argument  
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
